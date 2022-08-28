2b67d6003002af71be51ef09c8d4e36ad69701e1
Fatima Ashraf <109843582+Fatima0017@users.noreply.github.com>
2022-08-28 05:53:54 +0500

    Add files via upload

---
+++ ERC20.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract MyToken{
+
+    address public contractOwner;
+    mapping(address=>uint) public balance;
+     constructor(){
+         contractOwner=msg.sender;
+     }
+     function addToken(address whoAddToken, uint noOfTokens)public{
+         require(contractOwner==msg.sender);
+         balance[whoAddToken]+=noOfTokens;
+
+     }
+     function send(address rec, uint amount) public{
+         require(amount<=balance[msg.sender],"Insufficient amount");
+         balance[msg.sender]-=amount;
+         balance[rec]+=amount;
+     }
+
+    
+
+}
